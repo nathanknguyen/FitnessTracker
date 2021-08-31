1a02e7da7104d10a3992bc4e32ba5cc0c5b7e3d9
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-08-31 13:35:54 -0700

    api routes

---
+++ routes/api.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ day: -1 })
+    .limit(7)
+    .sort({ day: 1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: { exercises: req.body },
+    },
+    (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+});
+module.exports = router;
